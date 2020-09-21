888451efe7b1189bf262f0c032e229438133e804
alfianjau <alfianjauhari@gmail.com>
2020-09-22 06:24:06 +0700

    feat: add dynamic import on typescript from component folder

--- pages/request.tsx
+++ pages/request.tsx
@@ -1,19 +1,20 @@
 import axios from 'axios'
+import { AppProps } from 'next/dist/next-server/lib/router/router'
 
-const Request = ({ restaurants, error }) => {
+const Request = ({ restaurants, error }: AppProps) => {
   if (error) {
     return <div>An error occured: {error.message}</div>
   }
   return (
     <ul>
-      {restaurants.map((restaurant) => (
+      {restaurants.map((restaurant: AppProps) => (
         <li key={restaurant.id}>{restaurant.name}</li>
       ))}
     </ul>
   )
 }
 
-Request.getInitialProps = async (ctx) => {
+Request.getInitialProps = async (ctx: AppProps) => {
   try {
     const res = await axios.get(`${process.env.BASE_URL}restaurants`)
     const restaurants = res.data
